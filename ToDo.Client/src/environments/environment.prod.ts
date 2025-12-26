6e20796b085a065288a6c0094ee76f454fd0ee16
Ranga <grpkarunathilaka@gmail.com>
2025-12-26 21:40:46 +1100

    Update production API URL in environment file

--- ToDo.Client/src/environments/environment.prod.ts
+++ ToDo.Client/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://api.azurewebsites.net/api'
+  apiUrl: 'https://todo-api-ranga-6741-ata6b9g4czcqhqeh.australiasoutheast-01.azurewebsites.net/api'
 };
